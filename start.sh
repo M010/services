77e2c0b0d60733a73e9ccb7a4780ff613867c3bf
m010 <sbashir@vo-k7.msk.21-school.ru>
2020-11-11 23:45:36 +0300

    clean

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+minikube delete
 minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
+minikube addons enable metallb
+alias klaf="kubectl apply -f"
+klaf ./srcs/yamll/metallb-config.yaml
+klaf apply -f /srcs/yamll
+docker build ./srcs/Nginx -t my_nginx
+klaf ./srcs/Nginx/nginx-deployment.yaml
