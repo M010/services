e2c2c521b6ca6fec731f8d2dd989ca2a89d5a529
m010 <sbashir@ox-c3.msk.21-school.ru>
2020-11-13 16:29:11 +0300

    working with nginx (-)

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 minikube delete
 minikube start --vm-driver=virtualbox
+cd srcs
 eval $(minikube docker-env)
 minikube addons enable metallb
 alias klaf="kubectl apply -f"
-klaf ./srcs/yamll/metallb-config.yaml
-klaf apply -f /srcs/yamll
-docker build ./srcs/Nginx -t my_nginx
-klaf ./srcs/Nginx/nginx-deployment.yaml
+kubectl apply -f ./yamll/metallb-config.yaml
+kubectl apply -f yamll
+docker build ./Nginx -t my_nginx
+kubectl apply -f  ./Nginx/nginx-deployment.yaml
