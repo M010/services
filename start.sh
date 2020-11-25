f6a6d28300065eba153ec3c407de32b76b19681b
m010 <sbashir@ox-d5.msk.21-school.ru>
2020-11-25 19:30:01 +0300

    start.sh with confirm and working basic grafana

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
 minikube delete
 minikube start --vm-driver=virtualbox
 cd srcs
@@ -9,3 +13,4 @@ kubectl apply -f ./yamll/metallb-config.yaml
 kubectl apply -f yamll
 docker build ./Nginx -t my_nginx
 kubectl apply -f  ./Nginx/nginx-deployment.yaml
+fi
