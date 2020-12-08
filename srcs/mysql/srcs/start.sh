ff77b8c3dceb0ffba5f468cee3de1d820fe5f254
moh <you@example.com>
2020-12-08 20:19:02 +0300

    mysql working with dump, recreate porblems

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -2,8 +2,6 @@ openrc default
 service mariadb setup
 service mariadb start
 mysql < create_wordpress_db.sql
-#mysql -u        root --skip-password -e "CREATE DATABASE wordpress;"
-#mysql -u        root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" &2>/dev/null
-#mysql -u        root --skip-password -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('admin');FLUSH PRIVILEGES;" &2> /dev/null 
+mysql wordpress < wp.sql
 sleep infinity
 wait
