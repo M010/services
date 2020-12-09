1318c43c0e09ac1cc603eda91f5b63e3073ab85d
moh <you@example.com>
2020-12-09 03:07:19 +0300

    mysql totaly working

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
+
 openrc default
-service mariadb setup
-service mariadb start
-mysql < create_wordpress_db.sql
-mysql wordpress < wp.sql
-sleep infinity
-wait
+if [ ! -d "/var/lib/mysql/wordpress" ];
+then
+    service mariadb setup
+    service mariadb start
+    mysql < create_wordpress_db.sql
+    mysql wordpress < wp.sql
+    service mariadb stop
+fi
+mysqld_safe
