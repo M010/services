fb4810da117e061316522f65edfbc3b0bee63f4a
moh <you@example.com>
2020-12-09 04:48:57 +0300

    Phpmyadmin with multiple processes

--- srcs/phpmyadmin/srcs/start.sh
+++ srcs/phpmyadmin/srcs/start.sh
@@ -1,5 +1,16 @@
 openrc          default
 service   	php-fpm7	start
 service         nginx		start
-sleep infinity
-wait
+
+while sleep 1; do
+  ps aux |grep php-fpm7 |grep -q -v grep
+  PHP_STATUS=$?
+  ps aux |grep nginx |grep -q -v grep
+  NGINX_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PHP_STATUS -ne 0 -o $NGINX_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
+done
