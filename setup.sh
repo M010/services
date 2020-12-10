a300dce7642b0d7b4dad12ee9846e03a337ab2aa
moh <you@example.com>
2020-12-10 16:30:22 +0300

    Cute setupsh

--- setup.sh
+++ setup.sh
@@ -12,15 +12,17 @@ minikube start --vm-driver=virtualbox
 cd srcs
 eval $(minikube docker-env)
 minikube addons enable metallb
+minikube addons enable metrics-server;
+minikube addons enable dashboard;
 
-kubectl apply -f ./yamll/metallb-config.yaml
-kubectl apply -f yamll
+
+kubectl apply -f ./metallb-config.yaml
+#kubectl apply -f yamll
 
 DIRS="mysql wordpress nginx phpmyadmin ftps influxdb telegraf grafana"
 
 for dir in $DIRS
 do
-	docker build ./$dir -t my_$dir
-	kubectl apply -f  ./$dir
-        echo -e "\e[32 $dir configured\e[0m"
+	docker build ./$dir -t my_$dir && echo -e "🐋 \e[33m $dir IMAGE created\n \e[0m"
+	kubectl apply -f  ./$dir       &&  echo -e "😎 \e[33m  $dir deployed to k8s\n \e[0m"
 done
